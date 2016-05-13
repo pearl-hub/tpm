1a58e778694c4b25684a2f9290c2fcf75c3653fd
Filippo Squillace <squillace.filippo@gmail.com>
2016-05-13 23:38:04 +0100

    Do not fail fast in case the tmux.conf sourcing fails

--- pearl-metadata/install.sh
+++ pearl-metadata/install.sh
@@ -3,8 +3,11 @@ _initialize_config() {
     apply "# Initialize TMUX plugin manager (keep this line at the very bottom of tmux.conf)" ${HOME}/.tmux.conf false
     link tmux $PEARL_PKGDIR/tpm.conf false
 
-    # If tmux server is running source the config file
-    tmux info &> /dev/null && tmux source ${HOME}/.tmux.conf
+    # If tmux server is running, source the config file
+    if tmux info &> /dev/null
+    then
+        tmux source ${HOME}/.tmux.conf || warn "Warn: Could not source ~/.tmux.conf"
+    fi
 
     info "Press 'prefix + I' inside a tmux session to install/update the initial plugins"
 }
